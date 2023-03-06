3e56c39410591535e111e37378e980c692dd595a
최서윤 <cinnamon1995@naver.com>
2023-03-07 01:49:17 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Box1 from './components/Box1'
+import Box2 from './components/Box2'
+import Box3 from './components/Box3'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const boxesStyle = {
+  display: 'flex',
+  marginTop: '10px',
 }
 
-export default App;
+function App() {
+  console.log('App 컴포넌트가 렌더링되었습니다!')
+
+  const [count, setCount] = useState(0)
+
+  // 1을 증가시키는 함수
+  const onPlusButtonClickHandler = () => {
+    setCount(count + 1)
+  }
+
+  // 1을 감소시키는 함수
+  const onMinusButtonClickHandler = () => {
+    setCount(count - 1)
+  }
+
+  return (
+    <>
+      <h3>카운트 예제입니다!</h3>
+      <p>현재 카운트 : {count}</p>
+      <button onClick={onPlusButtonClickHandler}>+</button>
+      <button onClick={onMinusButtonClickHandler}>-</button>
+      <div style={boxesStyle}>
+        <Box1 />
+        <Box2 />
+        <Box3 />
+      </div>
+    </>
+  )
+}
+
+export default App
